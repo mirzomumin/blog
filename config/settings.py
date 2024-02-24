9a7c6d76f50e6694a73d407a7a829f818818ce1c
Mirzomumin <mirzomumin@list.ru>
2024-02-24 15:05:08 +0400

    Add secrets to environment variable

--- config/settings.py
+++ config/settings.py
@@ -10,20 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s^hcj(b5sml!-n6fepq9tm_k(y(##*xmrjsx6sca&dctfjj#zx"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ["DEBUG"])
 
 ALLOWED_HOSTS = []
 
@@ -128,8 +133,8 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Email server configuration
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "mirzomumin98@gmail.com"
-EMAIL_HOST_PASSWORD = "lnfptrfhjckzmbjs"
+EMAIL_HOST = os.environ["EMAIL_HOST"]
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
