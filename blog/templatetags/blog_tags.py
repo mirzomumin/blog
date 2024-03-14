2d69bb8be4359ecd299f33c83f2c68091a35a49b
Mirzomumin <mirzomumin@list.ru>
2024-03-14 10:20:05 +0400

    Add custom manager for post model

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,25 +8,19 @@ register = template.Library()
 
 @register.simple_tag
 def total_posts():
-    return Post.objects.filter(
-        status=Post.Status.PUBLISHED,
-    ).count()
+    return Post.objects.published().count()
 
 
 @register.inclusion_tag("blog/post/latest_posts.html")
 def show_latest_posts(count=5):
-    latest_posts = Post.objects.filter(
-        status=Post.Status.PUBLISHED,
-    ).order_by("-published_at")[:count]
+    latest_posts = Post.objects.published().order_by("-published_at")[:count]
     return {"latest_posts": latest_posts}
 
 
 @register.simple_tag
 def get_most_commented_posts(count=5):
     return (
-        Post.objects.filter(
-            status=Post.Status.PUBLISHED,
-        )
+        Post.objects.published()
         .annotate(total_comments=Count("comments"))
         .order_by("-total_comments")[:count]
     )
