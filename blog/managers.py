76e5b6f089d5dae1626f0cad876722dab5427f1c
Mirzomumin <mirzomumin@list.ru>
2024-07-28 21:43:09 +0500

    add comment to manager method

--- blog/managers.py
+++ blog/managers.py
@@ -3,6 +3,7 @@ from django.db.models import Manager
 
 class PostManager(Manager):
     def published(self, **kwargs):
+        # return published posts
         return self.filter(
             status=self.model.Status.PUBLISHED,
             **kwargs,
